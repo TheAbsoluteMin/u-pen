ae0752b7d77d69cfff25e408fc9853075dc551f7
TheAbsoluteMin <phdhminh@gmail.com>
2026-07-12 13:22:42 +0000

    Create new file.

---
+++ main.ino
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SparkFun_MAX17048_Arduino_Library.h>
+#include <LSM6DSV16XSensor.h>
